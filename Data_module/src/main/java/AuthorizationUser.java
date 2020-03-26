5fa5a9918cc8086664ad2cd7c889528303e10974
dell <vladsinitsa23@gmail.com>
2020-03-26 15:53:17 +0300

    Started working with Data_module

---
+++ Data_module/src/main/java/AuthorizationUser.java
@@ -0,0 +1,25 @@
+public class AuthorizationUser {
+    private String login;
+    private String password;
+
+    public AuthorizationUser(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

6a590c7467e9f6a4b126be3503747ea553ae6174
dell <vladsinitsa23@gmail.com>
2020-03-26 21:17:46 +0300

    Addition in: Service and Web modules

--- Service_module/src/main/java/IUserService.java
+++ Service_module/src/main/java/IUserService.java
@@ -1,4 +1,7 @@
+import java.util.List;
+
 public interface IUserService {
+    List<User> getUsers();
     String saveUser(User user);
     void saveAuthorizationUser(AuthorizationUser authorizationUser);
 }
--- Web_module/src/main/java/WebUtils.java
+++ Web_module/src/main/java/WebUtils.java
@@ -10,7 +10,7 @@ public class WebUtils {
     private static final Logger logger = LoggerFactory.getLogger(WebUtils.class);
     public static void forword(String page, HttpServletRequest request, HttpServletResponse response){
         try {
-            request.getRequestDispatcher("/"+page+".jsp").forward(request, response);
+            request.getRequestDispatcher("/" + page + ".jsp").forward(request, response);
         } catch (ServletException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {

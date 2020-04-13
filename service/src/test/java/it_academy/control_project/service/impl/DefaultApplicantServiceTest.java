0150fc22812ca36d95965159fba20a080f71e686
dell <vladsinitsa23@gmail.com>
2020-04-13 12:50:56 +0300

    We begin to create filters

--- service/src/test/java/it_academy/control_project/service/impl/DefaultApplicantServiceTest.java
+++ service/src/test/java/it_academy/control_project/service/impl/DefaultApplicantServiceTest.java
@@ -23,46 +23,46 @@ class DefaultApplicantServiceTest {
 
     @Test
     void getNonExistentUser(){
-        when(dao.getApplicant(0)).thenReturn(null);
-        Applicant applicant = service.getApplicant(0);
+        when(dao.getApplicant(0L)).thenReturn(null);
+        Applicant applicant = service.getApplicant(0L);
         assertNull(applicant);
     }
 
     @Test
     void getExistingUser(){
-        when(dao.getApplicant(1)).thenReturn(new Applicant(1L, 1L, 1L, 10));
-        Applicant applicantFromDb = service.getApplicant(1);
+        when(dao.getApplicant(1L)).thenReturn(new Applicant(1L, 1L, 1L, 10));
+        Applicant applicantFromDb = service.getApplicant(1L);
         assertNotNull(applicantFromDb);
         assertEquals(applicantFromDb.getMark(), 10);
     }
 
     @Test
     void testLoginWrongPass() {
-        when(dao.getApplicant(1)).thenReturn(new Applicant(1L, 1L, 1L, 10));
-        Applicant applicantFromDb = service.getApplicant(2);
+        when(dao.getApplicant(1L)).thenReturn(new Applicant(1L, 1L, 1L, 10));
+        Applicant applicantFromDb = service.getApplicant(2L);
         assertNull(applicantFromDb);
     }
 
     @Test
     void deleteUser(){
-        when(dao.getApplicant(1)).thenReturn(new Applicant(1L, 1L, 1L, 10));
-        Applicant applicantFromDb = service.getApplicant(1);
+        when(dao.getApplicant(1L)).thenReturn(new Applicant(1L, 1L, 1L, 10));
+        Applicant applicantFromDb = service.getApplicant(1L);
         assertNotNull(applicantFromDb);
 
-        final boolean deleted = service.deleteApplicant(1);
+        final boolean deleted = service.deleteApplicant(1L);
         assertTrue(deleted);
 
-        final Applicant afterDeleted = service.getApplicant(1);
+        final Applicant afterDeleted = service.getApplicant(1L);
         assertNull(afterDeleted);
     }
 
     @Test
     void getUser(){
-        when(dao.getApplicant(1)).thenReturn(new Applicant(1L, 1L, 1L, 10));
-        Applicant applicantFromDb = service.getApplicant(1);
+        when(dao.getApplicant(1L)).thenReturn(new Applicant(1L, 1L, 1L, 10));
+        Applicant applicantFromDb = service.getApplicant(1L);
         assertNotNull(applicantFromDb);
 
-        final Applicant applicant = service.getApplicant(1);
+        final Applicant applicant = service.getApplicant(1L);
         assertNotNull(applicant);
         assertEquals(applicantFromDb.getId(), applicant.getId());
         assertEquals(applicantFromDb.getUserId(), applicant.getUserId());
